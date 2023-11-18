3e05e3842e05ab3dfb7a13ed4f5f3b4e394cd662
E021ntox <136792100+E021ntox@users.noreply.github.com>
2023-11-18 07:39:36 +0000

    Create global_yahei.user.js

---
+++ userscripts/global_yahei.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Global YaHei
+// @version      0.1
+// @description
+// @include      *
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+  const css = `
+
+@font-face { font-family: "SimSun"; src: local("Microsoft YaHei UI"); }
+@font-face { font-family: "NSimSun"; src: local("Microsoft YaHei UI"); }
+@font-face { font-family: "宋体"; src: local("Microsoft YaHei UI"); }
+
+@font-face { font-family: "MS PGothic"; src: local("Yu Gothic"); }
+
+`
+
+  GM_addStyle(css)
+
+})()
