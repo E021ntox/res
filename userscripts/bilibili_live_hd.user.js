3489e1a52154a32d4f4ae291a7769151149c9fdd
E021ntox <136792100+E021ntox@users.noreply.github.com>
2024-03-07 17:06:15 +0000

    Update bilibili_live_hd.user.js

--- userscripts/bilibili_live_hd.user.js
+++ userscripts/bilibili_live_hd.user.js
@@ -2,8 +2,8 @@
 // @name         BiliBili Live HD
 // @version      0.1
 // @description
-// @icon         https://www.bilibili.com/favicon.ico
 // @include      https://live.bilibili.com/*
+// @icon         https://www.bilibili.com/favicon.ico
 // @updateURL    https://github.com/E021ntox/res/raw/main/userscripts/bilibili_live_hd.user.js
 // @downloadURL  https://github.com/E021ntox/res/raw/main/userscripts/bilibili_live_hd.user.js
 // ==/UserScript==
